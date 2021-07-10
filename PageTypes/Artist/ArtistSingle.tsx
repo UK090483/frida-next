f0457a673406ced6a15c4b7a6a755c1486e9757e
uk090483 <konradullrich@me.com>
2021-07-10 10:34:13 +0200

    no log

--- PageTypes/Artist/ArtistSingle.tsx
+++ PageTypes/Artist/ArtistSingle.tsx
@@ -19,8 +19,6 @@ const ArtistSingle: React.FC<ArtistSingleProps> = (props) => {
   const { relatedArtworks, lang, content, mainImage, initBgColor, quotes } =
     props
 
-  console.log(props)
-
   const _initBgColor = initBgColor ? initBgColor : 'white'
 
   const heroImage =
